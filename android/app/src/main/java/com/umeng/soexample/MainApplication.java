901aa710e234068b9df068ab989ab93e3e1fe36c
yajie.fyj <yajie.fyj@umeng.com>
2017-09-12 10:21:02 +0800

    接入push

--- android/app/src/main/java/com/umeng/soexample/MainApplication.java
+++ android/app/src/main/java/com/umeng/soexample/MainApplication.java
@@ -1,62 +1,174 @@
 package com.umeng.soexample;
 
-import android.app.Application;
+import java.util.Arrays;
+import java.util.List;
 
+import android.app.Application;
+import android.app.Notification;
+import android.content.Context;
+import android.os.Handler;
+import android.widget.RemoteViews;
+import android.widget.Toast;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
-import com.umeng.analytics.MobclickAgent;
-import com.umeng.analytics.MobclickAgent.EScenarioType;
 import com.umeng.commonsdk.UMConfigure;
+import com.umeng.message.IUmengRegisterCallback;
+import com.umeng.message.MsgConstant;
+import com.umeng.message.PushAgent;
+import com.umeng.message.UTrack;
+import com.umeng.message.UmengMessageHandler;
+import com.umeng.message.UmengNotificationClickHandler;
+import com.umeng.message.common.UmLog;
+import com.umeng.message.entity.UMessage;
 import com.umeng.socialize.PlatformConfig;
-import com.umeng.socialize.UMShareAPI;
 import com.umeng.soexample.invokenative.DplusReactPackage;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    private static final String TAG = MainApplication.class.getName();
+    private Handler handler;
+
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                new MainReactPackage(),
+                new DplusReactPackage()
+            );
+        }
+    };
+
     @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new DplusReactPackage()
-      );
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
+        UMConfigure.setLogEnabled(true);
+        //初始化组件化基础库, 统计SDK/推送SDK/分享SDK都必须调用此初始化接口
+        UMConfigure.init(this, "59892f08310c9307b60023d0", "Umeng", UMConfigure.DEVICE_TYPE_PHONE,
+            "669c30a9584623e70e8cd01b0381dcb4");
+        initUpush();
+
     }
-  };
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
+    private void initUpush() {
+        PushAgent mPushAgent = PushAgent.getInstance(this);
+        handler = new Handler(getMainLooper());
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-    UMConfigure.setLogEnabled(true);
-    //初始化组件化基础库, 统计SDK/推送SDK/分享SDK都必须调用此初始化接口
-    UMConfigure.init(this, "59892f08310c9307b60023d0", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, "669c30a9584623e70e8cd01b0381dcb4");
+        //sdk开启通知声音
+        mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SDK_ENABLE);
+        // sdk关闭通知声音
+        //		mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SDK_DISABLE);
+        // 通知声音由服务端控制
+        //		mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SERVER);
+
+        //		mPushAgent.setNotificationPlayLights(MsgConstant.NOTIFICATION_PLAY_SDK_DISABLE);
+        //		mPushAgent.setNotificationPlayVibrate(MsgConstant.NOTIFICATION_PLAY_SDK_DISABLE);
 
 
-  }
-  {
+        UmengMessageHandler messageHandler = new UmengMessageHandler() {
+            /**
+             * 自定义消息的回调方法
+             */
+            @Override
+            public void dealWithCustomMessage(final Context context, final UMessage msg) {
 
-    PlatformConfig.setWeixin("wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0");
-    //豆瓣RENREN平台目前只能在服务器端配置
-    PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad","http://sns.whalecloud.com");
-    PlatformConfig.setYixin("yxc0614e80c9304c11b0391514d09f13bf");
-    PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+                handler.post(new Runnable() {
 
-  }
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        // 对自定义消息的处理方式，点击或者忽略
+                        boolean isClickOrDismissed = true;
+                        if (isClickOrDismissed) {
+                            //自定义消息的点击统计
+                            UTrack.getInstance(getApplicationContext()).trackMsgClick(msg);
+                        } else {
+                            //自定义消息的忽略统计
+                            UTrack.getInstance(getApplicationContext()).trackMsgDismissed(msg);
+                        }
+                        Toast.makeText(context, msg.custom, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            /**
+             * 自定义通知栏样式的回调方法
+             */
+            @Override
+            public Notification getNotification(Context context, UMessage msg) {
+                switch (msg.builder_id) {
+                    case 1:
+                        Notification.Builder builder = new Notification.Builder(context);
+                        RemoteViews myNotificationView = new RemoteViews(context.getPackageName(), R.layout.notification_view);
+                        myNotificationView.setTextViewText(R.id.notification_title, msg.title);
+                        myNotificationView.setTextViewText(R.id.notification_text, msg.text);
+                        myNotificationView.setImageViewBitmap(R.id.notification_large_icon, getLargeIcon(context, msg));
+                        myNotificationView.setImageViewResource(R.id.notification_small_icon, getSmallIconId(context, msg));
+                        builder.setContent(myNotificationView)
+                            .setSmallIcon(getSmallIconId(context, msg))
+                            .setTicker(msg.ticker)
+                            .setAutoCancel(true);
+
+                        return builder.getNotification();
+                    default:
+                        //默认为0，若填写的builder_id并不存在，也使用默认。
+                        return super.getNotification(context, msg);
+                }
+            }
+        };
+        mPushAgent.setMessageHandler(messageHandler);
+
+        /**
+         * 自定义行为的回调处理，参考文档：高级功能-通知的展示及提醒-自定义通知打开动作
+         * UmengNotificationClickHandler是在BroadcastReceiver中被调用，故
+         * 如果需启动Activity，需添加Intent.FLAG_ACTIVITY_NEW_TASK
+         * */
+        UmengNotificationClickHandler notificationClickHandler = new UmengNotificationClickHandler() {
+            @Override
+            public void dealWithCustomAction(Context context, UMessage msg) {
+                Toast.makeText(context, msg.custom, Toast.LENGTH_LONG).show();
+            }
+        };
+        //使用自定义的NotificationHandler，来结合友盟统计处理消息通知，参考http://bbs.umeng.com/thread-11112-1-1.html
+        //CustomNotificationHandler notificationClickHandler = new CustomNotificationHandler();
+        mPushAgent.setNotificationClickHandler(notificationClickHandler);
+
+
+        //注册推送服务 每次调用register都会回调该接口
+        mPushAgent.register(new IUmengRegisterCallback() {
+            @Override
+            public void onSuccess(String deviceToken) {
+                UmLog.i(TAG, "device token: " + deviceToken);
+            }
+
+            @Override
+            public void onFailure(String s, String s1) {
+                UmLog.i(TAG, "register failed: " + s + " " + s1);
+            }
+        });
+    }
+
+
+    {
+
+        PlatformConfig.setWeixin("wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0");
+        //豆瓣RENREN平台目前只能在服务器端配置
+        PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad", "http://sns.whalecloud.com");
+        PlatformConfig.setYixin("yxc0614e80c9304c11b0391514d09f13bf");
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+
+    }
 }
--- android/app/src/main/java/com/umeng/soexample/invokenative/PushModule.java
+++ android/app/src/main/java/com/umeng/soexample/invokenative/PushModule.java
@@ -1,26 +1,159 @@
 package com.umeng.soexample.invokenative;
 
 import android.app.Activity;
+import java.util.List;
+import android.os.Handler;
+import android.util.Log;
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.umeng.message.MsgConstant;
+import com.umeng.message.PushAgent;
+import com.umeng.message.UTrack;
+import com.umeng.message.common.UmengMessageDeviceConfig;
+import com.umeng.message.common.inter.ITagManager;
+import com.umeng.message.tag.TagManager;
 
 /**
- * Created by wangfei on 17/8/30.
+ * Created by wangfei on 17/8/30
  */
 
 public class PushModule extends ReactContextBaseJavaModule {
+
+    private static final String TAG = PushModule.class.getSimpleName();
+
     private ReactApplicationContext context;
     private boolean isGameInited = false;
     private static Activity ma;
+    private PushAgent mPushAgent;
+    private Handler handler;
+
     public PushModule(ReactApplicationContext reactContext) {
         super(reactContext);
         context = reactContext;
+        mPushAgent = PushAgent.getInstance(context);
     }
-    public static void initPushSDK(Activity activity){
+
+    public static void initPushSDK(Activity activity) {
         ma = activity;
     }
+
     @Override
     public String getName() {
         return "push";
+    }
+
+    @ReactMethod
+    public void addTag(String tag, final Callback successCallback) {
+        mPushAgent.getTagManager().addTags(new TagManager.TCallBack() {
+            @Override
+            public void onMessage(final boolean isSuccess, final ITagManager.Result result) {
+                successCallback.invoke("");
+                if (isSuccess) {
+                    successCallback.invoke("Add Tag:" + result);
+                } else {
+                    successCallback.invoke("Add Tag:" + "加入tag失败");
+                }
+
+            }
+        }, tag);
+    }
+
+    @ReactMethod
+    public void deleteTag(String tag, final Callback successCallback) {
+        mPushAgent.getTagManager().deleteTags(new TagManager.TCallBack() {
+            @Override
+            public void onMessage(boolean isSuccess, final ITagManager.Result result) {
+                Log.i(TAG, "isSuccess:" + isSuccess);
+                if (isSuccess) { Log.i(TAG, "deletTag was set successfully."); }
+                successCallback.invoke("delet Tags:" + (isSuccess ? "Success" : "Fail"));
+            }
+        }, tag);
+    }
+
+    @ReactMethod
+    public void listTag(final Callback successCallback) {
+        mPushAgent.getTagManager().getTags(new TagManager.TagListCallBack() {
+            @Override
+            public void onMessage(final boolean isSuccess, final List<String> result) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (isSuccess) {
+                            if (result != null) {
+                                StringBuilder info = new StringBuilder();
+                                info.append("Tags:");
+                                for (int i = 0; i < result.size(); i++) {
+                                    String tag = result.get(i);
+                                    info.append("\n" + tag);
+                                }
+                                successCallback.invoke(info.toString());
+                            } else {
+                                successCallback.invoke("");
+                            }
+                        } else {
+                            successCallback.invoke("Tags获取失败");
+                        }
+
+                    }
+                });
+
+            }
+        });
+    }
+
+    @ReactMethod
+    public void addAlias(String alias, String aliasType, final Callback successCallback) {
+        mPushAgent.addAlias(alias, aliasType, new UTrack.ICallBack() {
+            @Override
+            public void onMessage(boolean isSuccess, String message) {
+                Log.i(TAG, "isSuccess:" + isSuccess + "," + message);
+                if (isSuccess) { Log.i(TAG, "alias was set successfully."); }
+                successCallback.invoke("", "", "Add Alias:" + (isSuccess ? "Success" : "Fail"));
+            }
+        });
+    }
+
+    @ReactMethod
+    public void addAliasType() {
+
+    }
+
+    @ReactMethod
+    public void addExclusiveAlias(String exclusiveAlias, String aliasType, final Callback successCallback) {
+        mPushAgent.setAlias(exclusiveAlias, aliasType, new UTrack.ICallBack() {
+            @Override
+            public void onMessage(boolean isSuccess, String message) {
+                Log.i(TAG, "isSuccess:" + isSuccess + "," + message);
+                if (Boolean.TRUE.equals(isSuccess)) {
+                    Log.i(TAG, "Exclusive alias was set successfully.");
+                }
+                successCallback.invoke("", "Add Exclusive Alias:" + (isSuccess ? "Success" : "Fail"));
+            }
+        });
+    }
+
+    @ReactMethod
+    public void deleteAlias(String alias, String aliasType, final Callback successCallback) {
+        mPushAgent.deleteAlias(alias, aliasType, new UTrack.ICallBack() {
+            @Override
+            public void onMessage(boolean isSuccess, String s) {
+                if (Boolean.TRUE.equals(isSuccess)) {
+                    Log.i(TAG, "Delete alias was set successfully.");
+                }
+
+                successCallback.invoke("", "", "Delete Alias:" + (isSuccess ? "Success" : "Fail"));
+            }
+        });
+    }
+
+    @ReactMethod
+    public void appInfo(final Callback successCallback) {
+        String pkgName = context.getPackageName();
+        String info = String.format("DeviceToken:%s\n" + "SdkVersion:%s\nAppVersionCode:%s\nAppVersionName:%s",
+            mPushAgent.getRegistrationId(), MsgConstant.SDK_VERSION,
+            UmengMessageDeviceConfig.getAppVersionCode(context), UmengMessageDeviceConfig.getAppVersionName(context));
+        successCallback.invoke("应用包名:" + pkgName + "\n" + info);
     }
 }
